9a289ecd757325e94bb021e5e1984c1bb6fd2746
mnowak <novvak.michal04@gmail.com>
2020-06-09 14:43:12 +0200

    Add codes of numerical methods.

---
+++ numerical-methods/Bisection.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+
+constexpr int DIVIDE_COUNT = 5;
+
+int main()
+  {
+  std::cout << "Find root of equation: x*x - 5 = 0 in interval [0, 10]." << std::endl;
+  std::cout << std::endl;
+
+  auto equation = [](double &aX)
+    {
+    return (aX * aX) - 5;
+    };
+
+  double leftOfInterval = 0;
+  double rightOfInterval = 10;
+  double root;
+  for (int i = 0; i < DIVIDE_COUNT; i++)
+    {
+    root = (leftOfInterval + rightOfInterval) * 0.5;
+    if (equation(root) == 0)
+      break;
+
+    if (equation(root) * equation(leftOfInterval) < 0)
+      rightOfInterval = root;
+    else if (equation(root) * equation(rightOfInterval) < 0)
+      leftOfInterval = root;
+    }
+
+  std::cout << "Solution of equation -> " << root << std::endl;
+  std::cout << "Error -> " << std::abs(std::sqrt(5) - root) << std::endl;
+
+  return 0;
+  }
