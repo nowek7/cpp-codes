367a0354651c7ab6775331b03766846a9f7f3e0c
nowek7 <novvak.michal04@gmail.com>
2020-12-03 19:16:46 +0100

    Cleanup

--- miscellaneous/testing/TowerListExample.cpp
+++ miscellaneous/testing/TowerListExample.cpp
@@ -1,3 +1,6 @@
+#include <cstdio>
+#include <vector>
+
 class TowerList
   {
   public:
@@ -7,28 +10,28 @@ class TowerList
       theTowerHeights{25.0f, 44.0f, 12.0f}
       {}
 
-    auttower getMaxTowerHeight() const
+    auto getMaxTowerHeight() const
       {
       return theMaxHeightIdx >= 0 ? theTowerHeights[theMaxHeightIdx] : 0.0f;
       }
 
-    // Move constructor
     TowerList(TowerList &&aTowerList) noexcept
       {
+      printf("Move constructor\n");
       std::swap(theTowerHeights, aTowerList.theTowerHeights);
       std::swap(theMaxHeightIdx, aTowerList.theMaxHeightIdx);
       }
 
-    // Move assignment
     auto &operator=(TowerList &&aTowerList) noexcept
       {
+      printf("Move assignment\n");
       std::swap(theTowerHeights, aTowerList.theTowerHeights);
       std::swap(theMaxHeightIdx, aTowerList.theMaxHeightIdx);
       return *this;
       }
 
     std::vector<float> theTowerHeights{};
-    auto theMaxHeightIdx{-1};
+    int theMaxHeightIdx{-1};
   };
 
 int main()
