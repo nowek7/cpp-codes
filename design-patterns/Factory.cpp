f699cfc8ecc521fa4a84a9d61c8f9ff03c246706
nowek7 <novvak.michal04@gmail.com>
2020-12-08 22:08:31 +0100

    Add codes.

---
+++ design-patterns/Factory.cpp
@@ -0,0 +1,100 @@
+#include <typeinfo>
+#include <memory>
+#include <map>
+#include <functional>
+#include <string>
+#include <string_view>
+
+using namespace std::string_literals;
+
+class Image {};
+class BitmapImage : public Image {};
+class PngImage : public Image {};
+class JpgImage : public Image {};
+
+struct IImageFactory
+  {
+  virtual std::shared_ptr<Image> Create(std::string_view type) = 0;
+  };
+
+struct ImageFactoryClassic : public IImageFactory
+  {
+  virtual std::shared_ptr<Image> Create(std::string_view type) override
+    {
+    if (type == "bmp")
+      return std::make_shared<BitmapImage>();
+    else if (type == "png")
+      return std::make_shared<PngImage>();
+    else if (type == "jpg")
+      return std::make_shared<JpgImage>();
+
+    return nullptr;
+    }
+  };
+
+struct ImageFactory : public IImageFactory
+  {
+  virtual std::shared_ptr<Image> Create(std::string_view type) override
+    {
+    static std::map<std::string,
+                    std::function<std::shared_ptr<Image>()>> factoryMap
+      {
+        { "bmp", []() {return std::make_shared<BitmapImage>(); } },
+        { "png", []() {return std::make_shared<PngImage>(); } },
+        { "jpg", []() {return std::make_shared<JpgImage>(); } }
+      };
+
+    auto it = factoryMap.find(type.data());
+    if (it != factoryMap.end())
+      return it->second();
+
+    return nullptr;
+    }
+  };
+
+struct IImageFactoryByType
+  {
+  virtual std::shared_ptr<Image> Create(std::type_info const &type) = 0;
+  };
+
+struct ImageFactoryByType : public IImageFactoryByType
+  {
+  virtual std::shared_ptr<Image> Create(std::type_info const &type) override
+    {
+    auto it = factoryMap.find(&type);
+    if (it != factoryMap.end())
+      return it->second();
+
+    return nullptr;
+    }
+
+  private:
+    static std::map<std::type_info const*,
+                    std::function<std::shared_ptr<Image>()>> factoryMap;
+  };
+
+std::map<std::type_info const *,
+         std::function<std::shared_ptr<Image>()>> ImageFactoryByType::factoryMap
+  {
+    { &typeid(BitmapImage), []() {return std::make_shared<BitmapImage>(); } },
+    { &typeid(PngImage),    []() {return std::make_shared<PngImage>(); } },
+    { &typeid(JpgImage),    []() {return std::make_shared<JpgImage>(); } }
+  };
+
+int main()
+  {
+    {
+    auto factory = ImageFactoryClassic{};
+    auto image = factory.Create("png");
+    }
+
+    {
+    auto factory = ImageFactory{};
+    auto image = factory.Create("png");
+    }
+
+    {
+    auto factory = ImageFactoryByType{};
+    auto movie = factory.Create(typeid(PngImage));
+    }
+  }
---
+++ design-patterns/Singleton.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <assert.h>
+
+class Singleton
+  {
+  private:
+    Singleton() {}
+  public:
+    Singleton(Singleton const &) = delete;
+    Singleton& operator=(Singleton const&) = delete;
+
+    static Singleton& instance()
+      {
+      static Singleton single;
+      return single;
+      }
+
+    void demo() { std::cout << "Demo using classic singleton" << std::endl; }
+  };
+
+template <class T>
+class SingletonBase
+  {
+  protected:
+    SingletonBase() {}
+  public:
+    SingletonBase(const SingletonBase&) = delete;
+    SingletonBase& operator=(const SingletonBase&) = delete;
+
+    static T& instance()
+      {
+      static T single;
+      return single;
+      }
+  };
+
+class Single : public SingletonBase<Single>
+  {
+  Single() {}
+  friend class SingletonBase<Single>;
+
+  public:
+    void demo() { std::cout << "Demo using CRTP singleton" << std::endl; }
+  };
+
+int main()
+  {
+    {
+    auto &single = Singleton::instance();
+    single.demo();
+
+    auto &sec = Singleton::instance();
+    assert(&sec == &single);
+    }
+
+    {
+    auto &single = Single::instance();
+    single.demo();
+
+    auto &sec = Single::instance();
+    assert(&sec == &single);
+    }
+  }
