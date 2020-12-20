35f0d12430a922f690256e5af3c5846f928bca2c
nowek7 <novvak.michal04@gmail.com>
2020-12-20 18:09:53 +0100

    Add codes

--- miscellaneous/examples/variadic-templates-example.cpp
+++ miscellaneous/examples/variadic-templates-example.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 template <typename... Ts>
 auto add(Ts... args)
@@ -42,6 +43,30 @@ constexpr auto min(Ts&&... args)
   return (wrapper_min<Ts>{args} < ...).value;
   }
 
+template <typename F, typename T1, typename T2>
+auto fold(F&& f, T1 v1, T2 v2)
+  {
+  return f(v1, v2);
+  }
+
+template <typename F, typename T, typename... Ts>
+auto fold(F&& f, T head, Ts... tail)
+  {
+  return f(head, fold(std::forward<F>(f), tail...));
+  }
+
+template <typename T>
+bool isAny(T value, T elem)
+  {
+  return value == elem;
+  }
+
+template <typename T, typename... Ts>
+bool isAny(T value, T first, Ts... rest)
+  {
+  return value == first || isAny(value, rest...);
+  }
+
 int main()
   {
   std::cout << min(10, 2, 3, 4, 5) << std::endl;
@@ -57,4 +82,15 @@ int main()
   std::cout << anyOf(true, true, true) << std::endl;
   std::cout << anyOf(false, false, false) << std::endl;
   std::cout << anyOf(true, false, true) << std::endl;
+
+    {
+      auto n = fold(std::plus<>(), 1, 2, 3, 4, 5);
+      auto s = fold(std::plus<>(), "hello", " ", "world", "!");
+    }
+
+
+    {
+      auto i1 = isAny(42, 1, 23, 76, 44, 5);
+      auto a = 0;
+    }
   }
