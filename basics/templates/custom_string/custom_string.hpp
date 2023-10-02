2185540c0c019e6defff35c9e688b18f8989d09c
nowek7 <mnowak@duck.com>
2023-10-02 21:35:54 +0200

    common: Standarize code formatting
    Standarize code formatting to adhere to coding style guidelines.
    
    Title: Standarize code formatting
    Change-type: ImplementationChange
    Signed-off-by: mnowak@duck.com

--- basics/templates/custom_string/custom_string.hpp
+++ basics/templates/custom_string/custom_string.hpp
@@ -1,175 +1,153 @@
 #pragma once
 
-#include <type_traits>
 #include <algorithm>
 #include <cstring>
 #include <string>
+#include <type_traits>
 
 template<typename Char>
 class String
 {
-    public:
-        String(const char* chars)
-        {
-            const std::size_t length = strlen(chars);
+public:
+  String(const char* chars)
+  {
+    const std::size_t length = strlen(chars);
 
-            m_chars = new char[length + 1];
-            for (std::size_t i = 0; i < length; ++i)
-            {
-                m_chars[i] = chars[i];
-            }
-            m_chars[length] = '\0';
+    m_chars = new char[length + 1];
+    for (std::size_t i = 0; i < length; ++i) {
+      m_chars[i] = chars[i];
+    }
+    m_chars[length] = '\0';
+  }
+
+  String(std::string str):
+    String(str.c_str())
+  {
+  }
+
+  String(const String& rhs)
+  {
+    if (this != &rhs) {
+      delete[] m_chars;
+      m_chars = nullptr;
+      if (rhs.m_chars) {
+        const std::size_t length = rhs.length();
+
+        m_chars = new char[length + 1];
+        for (std::size_t i = 0; i < length; ++i) {
+          m_chars[i] = rhs.m_chars[i];
         }
+        m_chars[length] = '\0';
+      }
+    }
+  }
 
-        String(std::string str) :
-            String(str.c_str())
-        {
-        }
+  ~String()
+  {
+    if (m_chars) {
+      delete[] m_chars;
+      m_chars = nullptr;
+    }
+  }
 
-        String(const String& rhs)
-        {
-            if (this != &rhs)
-            {
-                delete[] m_chars;
-                m_chars = nullptr;
-                if (rhs.m_chars)
-                {
-                    const std::size_t length = rhs.length();
+  template<typename OtherChar>
+  String& operator+(const String<OtherChar>& rhs)
+  {
+    const std::size_t rhsLength = rhs.length();
+    if (rhsLength > 0) {
+      const std::size_t length = this->length();
+      char* tmp = new char[length + rhsLength];
 
-                    m_chars = new char[length + 1];
-                    for (std::size_t i = 0; i < length; ++i)
-                    {
-                        m_chars[i] = rhs.m_chars[i];
-                    }
-                    m_chars[length] = '\0';
-                }
-            }
-        }
+      std::size_t i = 0;
+      for (; i < length; ++i) {
+        tmp[i] = m_chars[i];
+      }
 
-        ~String()
-        {
-            if (m_chars)
-            {
-                delete[] m_chars;
-                m_chars = nullptr;
-            }
-        }
+      for (; i < length + rhsLength; ++i) {
+        tmp[i] = rhs.m_chars[i];
+      }
+      m_chars[length + rhsLength] = '\0';
 
-        template<typename OtherChar>
-        String& operator+(const String<OtherChar>& rhs)
-        {
-            const std::size_t rhsLength = rhs.length();
-            if (rhsLength > 0)
-            {
-                const std::size_t length = this->length();
-                char* tmp = new char[length + rhsLength];
+      delete[] m_chars;
+      m_chars = tmp;
+    }
+    return *this;
+  }
 
-                std::size_t i = 0;
-                for (; i < length; ++i)
-                {
-                    tmp[i] = m_chars[i];
-                }
+  bool operator==(const String& rhs)
+  {
+    if (this == &rhs) {
+      return true;
+    }
 
-                for (; i < length + rhsLength; ++i)
-                {
-                    tmp[i] = rhs.m_chars[i];
-                }
-                m_chars[length + rhsLength] = '\0';
+    if (length() != rhs.length()) {
+      return false;
+    }
 
-                delete[] m_chars;
-                m_chars = tmp;
-            }
-            return *this;
-        }
+    for (std::size_t i = 0; i < length(); ++i) {
+      if (m_chars[i] != rhs.m_chars[i]) {
+        return false;
+      }
+    }
 
-        bool operator==(const String& rhs)
-        {
-            if (this == &rhs)
-            {
-                return true;
-            }
+    return true;
+  }
 
-            if (length() != rhs.length())
-            {
-                return false;
-            }
+  bool operator!=(const String& rhs)
+  {
+    return !(*this == rhs);
+  }
 
-            for (std::size_t i = 0; i < length(); ++i)
-            {
-                if (m_chars[i] != rhs.m_chars[i])
-                {
-                    return false;
-                }
-            }
+  String& operator+=(Char c)
+  {
+    if (m_chars) {
+      const std::size_t length = this->length();
+      char* tmp = new char[length + 2];
 
-            return true;
-        }
+      for (std::size_t i = 0; i < length; ++i) {
+        tmp[i] = m_chars[i];
+      }
+      tmp[length] = c;
+      tmp[length + 1] = '\0';
 
-        bool operator!=(const String& rhs)
-        {
-            return !(*this == rhs);
-        }
+      delete[] m_chars;
+      m_chars = tmp;
+    } else {
+      m_chars = new char[2] {c, '\0'};
+    }
 
-        String& operator+=(Char c)
-        {
-            if (m_chars)
-            {
-                const std::size_t length = this->length();
-                char* tmp = new char[length + 2];
+    return *this;
+  }
 
-                for (std::size_t i = 0; i < length; ++i)
-                {
-                    tmp[i] = m_chars[i];
-                }
-                tmp[length] = c;
-                tmp[length + 1] = '\0';
+  Char& operator[](const int& idx)
+  {
+    if (m_chars) {
+      if (idx >= 0 && strlen(m_chars) > static_cast<std::size_t>(idx)) {
+        return m_chars[idx];
+      } else {
+        throw std::out_of_range("Index value not in expected range!");
+      }
+    } else {
+      throw std::logic_error("Unexpected access to uninitialized variable!");
+    }
+  }
 
-                delete[] m_chars;
-                m_chars = tmp;
-            }
-            else
-            {
-                m_chars = new char[2]{c, '\0'};
-            }
+  friend std::ostream& operator<<(std::ostream& os, const String<Char>& me)
+  {
+    os << me.m_chars;
+    return os;
+  }
 
-            return *this;
-        }
+  std::size_t length() const
+  {
+    return m_chars ? strlen(m_chars) : 0;
+  }
 
-        Char& operator[](const int& idx)
-        {
-            if (m_chars)
-            {
-                if (idx >= 0 && strlen(m_chars) > static_cast<std::size_t>(idx))
-                {
-                    return m_chars[idx];
-                }
-                else
-                {
-                    throw std::out_of_range("Index value not in expected range!");
-                }
-            }
-            else
-            {
-                throw std::logic_error("Unexpected access to uninitialized variable!");
-            }
-        }
+  bool empty()
+  {
+    return m_chars ? strlen(m_chars) == 0 : false;
+  }
 
-        friend std::ostream& operator<<(std::ostream& os, const String<Char>& me)
-        {
-            os << me.m_chars;
-            return os;
-        }
-
-        std::size_t length() const
-        {
-            return m_chars ? strlen(m_chars) : 0;
-        }
-
-        bool empty()
-        {
-            return m_chars ? strlen(m_chars) == 0 : false;
-        }
-
-    private:
-        Char* m_chars = nullptr;
+private:
+  Char* m_chars = nullptr;
 };
--- math_problem/algebra_varations/simple_matrix/matrix.hpp
+++ math_problem/algebra_varations/simple_matrix/matrix.hpp
@@ -49,11 +49,12 @@ private:
   double** theRows = nullptr;
 };
 
-Matrix::Matrix(const Matrix& aOther): theRowsNum(aOther.getRowsNum()), theColumnsNum(aOther.getColumnsNum())
+Matrix::Matrix(const Matrix& aOther):
+  theRowsNum(aOther.getRowsNum()),
+  theColumnsNum(aOther.getColumnsNum())
 {
   theRows = new double*[theRowsNum];
-  for (unsigned int i = 0; i < theRowsNum; i++)
-  {
+  for (unsigned int i = 0; i < theRowsNum; i++) {
     theRows[i] = new double[theColumnsNum];
     auto row = aOther.getRow(i);
     std::copy(row, row + theColumnsNum, theRows[i]);
@@ -64,18 +65,17 @@ Matrix::Matrix(std::initializer_list<std::initializer_list<double>> aRows)
 {
   const unsigned int rowsNum = static_cast<unsigned int>(aRows.size());
   const unsigned int columnsNum = static_cast<unsigned int>((*(aRows.begin())).size());
-  for (unsigned int i = 1; i < rowsNum; i++)
-  {
-    if ((*(aRows.begin() + i)).size() != columnsNum)
+  for (unsigned int i = 1; i < rowsNum; i++) {
+    if ((*(aRows.begin() + i)).size() != columnsNum) {
       throw std::logic_error("Size of columns is not the same!");
+    }
   }
 
   theRowsNum = rowsNum;
   theColumnsNum = columnsNum;
 
   theRows = new double*[theRowsNum];
-  for (unsigned int i = 0; i < rowsNum; i++)
-  {
+  for (unsigned int i = 0; i < rowsNum; i++) {
     auto row = *(aRows.begin() + i);
     theRows[i] = new double[theColumnsNum];
     std::copy(row.begin(), row.end(), theRows[i]);
@@ -83,11 +83,11 @@ Matrix::Matrix(std::initializer_list<std::initializer_list<double>> aRows)
 }
 
 Matrix::Matrix(const unsigned int& aRowsNum, const unsigned int& aColumnsNum, double** aRows):
-  theRowsNum(aRowsNum), theColumnsNum(aColumnsNum)
+  theRowsNum(aRowsNum),
+  theColumnsNum(aColumnsNum)
 {
   theRows = new double*[theRowsNum];
-  for (unsigned int i = 0; i < theRowsNum; i++)
-  {
+  for (unsigned int i = 0; i < theRowsNum; i++) {
     theRows[i] = new double[theColumnsNum];
     std::copy(aRows[i], aRows[i] + theColumnsNum, theRows[i]);
   }
@@ -99,8 +99,7 @@ Matrix Matrix::operator=(const Matrix& aOther)
   unsigned int columnsNum = aOther.getColumnsNum();
 
   double** rows = new double*[rowsNum];
-  for (unsigned int i = 0; i < rowsNum; i++)
-  {
+  for (unsigned int i = 0; i < rowsNum; i++) {
     rows[i] = new double[columnsNum];
     std::copy(rows[i], rows[i] + theColumnsNum, aOther.getRow(i));
   }
@@ -110,23 +109,25 @@ Matrix Matrix::operator=(const Matrix& aOther)
 
 Matrix::~Matrix()
 {
-  for (unsigned int i = 0; i < theRowsNum; i++)
+  for (unsigned int i = 0; i < theRowsNum; i++) {
     delete[] theRows[i];
+  }
   delete theRows;
 }
 
 inline Matrix Matrix::operator+(const Matrix& aOther)
 {
-  if ((theRowsNum != aOther.getRowsNum()) && (theColumnsNum != aOther.getColumnsNum()))
+  if ((theRowsNum != aOther.getRowsNum()) && (theColumnsNum != aOther.getColumnsNum())) {
     throw std::logic_error("Matrices have different size!");
+  }
 
   double** rows = new double*[theRowsNum];
-  for (unsigned int i = 0; i < theRowsNum; i++)
-  {
+  for (unsigned int i = 0; i < theRowsNum; i++) {
     rows[i] = new double[theColumnsNum];
     auto otherRow = aOther.getRow(i);
-    for (unsigned int j = 0; j < theColumnsNum; j++)
+    for (unsigned int j = 0; j < theColumnsNum; j++) {
       rows[i][j] = theRows[i][j] + otherRow[j];
+    }
   }
 
   return Matrix(theRowsNum, theColumnsNum, rows);
@@ -134,14 +135,15 @@ inline Matrix Matrix::operator+(const Matrix& aOther)
 
 Matrix& Matrix::operator+=(const Matrix& aOther)
 {
-  if ((theRowsNum != aOther.getRowsNum()) && (theColumnsNum != aOther.getColumnsNum()))
+  if ((theRowsNum != aOther.getRowsNum()) && (theColumnsNum != aOther.getColumnsNum())) {
     throw std::logic_error("Matrices have different size!");
+  }
 
-  for (unsigned int i = 0; i < theRowsNum; i++)
-  {
+  for (unsigned int i = 0; i < theRowsNum; i++) {
     auto otherRow = aOther.getRow(i);
-    for (unsigned int j = 0; j < theColumnsNum; j++)
+    for (unsigned int j = 0; j < theColumnsNum; j++) {
       theRows[i][j] += otherRow[j];
+    }
   }
 
   return *this;
@@ -149,16 +151,17 @@ Matrix& Matrix::operator+=(const Matrix& aOther)
 
 Matrix Matrix::operator-(const Matrix& aOther)
 {
-  if ((theRowsNum != aOther.getRowsNum()) && (theColumnsNum != aOther.getColumnsNum()))
+  if ((theRowsNum != aOther.getRowsNum()) && (theColumnsNum != aOther.getColumnsNum())) {
     throw std::logic_error("Matrices have different size!");
+  }
 
   double** rows = new double*[theRowsNum];
-  for (unsigned int i = 0; i < theRowsNum; i++)
-  {
+  for (unsigned int i = 0; i < theRowsNum; i++) {
     rows[i] = new double[theColumnsNum];
     auto otherRow = aOther.getRow(i);
-    for (unsigned int j = 0; j < theColumnsNum; j++)
+    for (unsigned int j = 0; j < theColumnsNum; j++) {
       rows[i][j] = theRows[i][j] - otherRow[j];
+    }
   }
 
   return Matrix(theRowsNum, theColumnsNum, rows);
@@ -166,14 +169,15 @@ Matrix Matrix::operator-(const Matrix& aOther)
 
 Matrix& Matrix::operator-=(const Matrix& aOther)
 {
-  if ((theRowsNum != aOther.getRowsNum()) && (theColumnsNum != aOther.getColumnsNum()))
+  if ((theRowsNum != aOther.getRowsNum()) && (theColumnsNum != aOther.getColumnsNum())) {
     throw std::logic_error("Matrices have different size!");
+  }
 
-  for (unsigned int i = 0; i < theRowsNum; i++)
-  {
+  for (unsigned int i = 0; i < theRowsNum; i++) {
     auto row = aOther.getRow(i);
-    for (unsigned int j = 0; j < theColumnsNum; j++)
+    for (unsigned int j = 0; j < theColumnsNum; j++) {
       theRows[i][j] -= row[j];
+    }
   }
 
   return *this;
@@ -183,18 +187,18 @@ Matrix Matrix::operator*(const Matrix& aOther)
 {
   unsigned int otherRowsNum = aOther.getRowsNum();
   unsigned int otherColumnsNum = aOther.getColumnsNum();
-  if (theColumnsNum != otherRowsNum)
+  if (theColumnsNum != otherRowsNum) {
     throw std::logic_error("Cannot multiply by matrix");
+  }
 
   double** rows = new double*[theRowsNum];
-  for (unsigned int i = 0; i < theRowsNum; i++)
-  {
+  for (unsigned int i = 0; i < theRowsNum; i++) {
     rows[i] = new double[otherColumnsNum];
-    for (unsigned int j = 0; j < otherColumnsNum; j++)
-    {
+    for (unsigned int j = 0; j < otherColumnsNum; j++) {
       double sum = 0;
-      for (unsigned int k = 0; k < otherRowsNum; k++)
+      for (unsigned int k = 0; k < otherRowsNum; k++) {
         sum += theRows[i][k] * aOther.getAt(k, j);
+      }
 
       rows[i][j] = sum;
     }
@@ -207,26 +211,25 @@ Matrix& Matrix::operator*=(const Matrix& aOther)
 {
   unsigned int otherRowsNum = aOther.getRowsNum();
   unsigned int otherColumnsNum = aOther.getColumnsNum();
-  if (theColumnsNum != otherRowsNum)
+  if (theColumnsNum != otherRowsNum) {
     throw std::logic_error("Cannot multiply by matrix");
+  }
 
   double** rows = new double*[theRowsNum];
-  for (unsigned int i = 0; i < theRowsNum; i++)
-  {
+  for (unsigned int i = 0; i < theRowsNum; i++) {
     rows[i] = new double[otherColumnsNum];
-    for (unsigned int j = 0; j < otherColumnsNum; j++)
-    {
+    for (unsigned int j = 0; j < otherColumnsNum; j++) {
       double sum = 0;
-      for (unsigned int k = 0; k < otherRowsNum; k++)
+      for (unsigned int k = 0; k < otherRowsNum; k++) {
         sum += theRows[i][k] * aOther.getAt(k, j);
+      }
 
       rows[i][j] = sum;
     }
   }
 
   theColumnsNum = otherColumnsNum;
-  for (unsigned int i = 0; i < theRowsNum; i++)
-  {
+  for (unsigned int i = 0; i < theRowsNum; i++) {
     delete[] theRows[i];
     theRows[i] = std::move(rows[i]);
   }
@@ -237,11 +240,11 @@ Matrix& Matrix::operator*=(const Matrix& aOther)
 Matrix Matrix::operator*(double aScalar)
 {
   double** rows = new double*[theRowsNum];
-  for (unsigned int i = 0; i < theRowsNum; i++)
-  {
+  for (unsigned int i = 0; i < theRowsNum; i++) {
     rows[i] = new double[theColumnsNum];
-    for (unsigned int j = 0; j < theColumnsNum; j++)
+    for (unsigned int j = 0; j < theColumnsNum; j++) {
       rows[i][j] = theRows[i][j] * aScalar;
+    }
   }
 
   return Matrix(theRowsNum, theColumnsNum, rows);
@@ -249,10 +252,10 @@ Matrix Matrix::operator*(double aScalar)
 
 Matrix& Matrix::operator*=(double aScalar)
 {
-  for (unsigned int i = 0; i < theRowsNum; i++)
-  {
-    for (unsigned int j = 0; j < theColumnsNum; j++)
+  for (unsigned int i = 0; i < theRowsNum; i++) {
+    for (unsigned int j = 0; j < theColumnsNum; j++) {
       theRows[i][j] *= aScalar;
+    }
   }
 
   return *this;
@@ -260,15 +263,16 @@ Matrix& Matrix::operator*=(double aScalar)
 
 Matrix Matrix::operator/(double aScalar)
 {
-  if (aScalar == 0)
+  if (aScalar == 0) {
     throw std::logic_error("Cannot divide by zero!");
+  }
 
   double** rows = new double*[theRowsNum];
-  for (unsigned int i = 0; i < theRowsNum; i++)
-  {
+  for (unsigned int i = 0; i < theRowsNum; i++) {
     rows[i] = new double[theColumnsNum];
-    for (unsigned int j = 0; j < theColumnsNum; j++)
+    for (unsigned int j = 0; j < theColumnsNum; j++) {
       rows[i][j] = theRows[i][j] / aScalar;
+    }
   }
 
   return Matrix(theRowsNum, theColumnsNum, rows);
@@ -276,13 +280,14 @@ Matrix Matrix::operator/(double aScalar)
 
 Matrix& Matrix::operator/=(double aScalar)
 {
-  if (aScalar == 0)
+  if (aScalar == 0) {
     throw std::logic_error("Cannot divide by zero!");
+  }
 
-  for (unsigned int i = 0; i < theRowsNum; i++)
-  {
-    for (unsigned int j = 0; j < theColumnsNum; j++)
+  for (unsigned int i = 0; i < theRowsNum; i++) {
+    for (unsigned int j = 0; j < theColumnsNum; j++) {
       theRows[i][j] /= aScalar;
+    }
   }
 
   return *this;
@@ -290,15 +295,16 @@ Matrix& Matrix::operator/=(double aScalar)
 
 inline bool Matrix::operator==(Matrix& aOther)
 {
-  if ((theRowsNum != aOther.getRowsNum()) && (theColumnsNum != aOther.getColumnsNum()))
+  if ((theRowsNum != aOther.getRowsNum()) && (theColumnsNum != aOther.getColumnsNum())) {
     return false;
+  }
 
-  for (unsigned int i = 0; i < theRowsNum; i++)
-  {
+  for (unsigned int i = 0; i < theRowsNum; i++) {
     auto row = aOther.getRow(i);
     auto res = std::equal(row, row + theColumnsNum, theRows[i], theRows[i] + theColumnsNum);
-    if (!res)
+    if (!res) {
       return res;
+    }
   }
 
   return true;
@@ -326,25 +332,30 @@ inline double** Matrix::getRows() const
 
 double* Matrix::getRow(unsigned int aRowIdx) const
 {
-  if (!theRows)
+  if (!theRows) {
     throw std::logic_error("The matrix is empty!");
+  }
 
-  if (aRowIdx > theRowsNum)
+  if (aRowIdx > theRowsNum) {
     throw std::out_of_range("Out of range rows!");
+  }
 
   return theRows[aRowIdx];
 }
 
 double Matrix::getAt(unsigned int aRowIdx, unsigned int aColIdx) const
 {
-  if (!theRows)
+  if (!theRows) {
     throw std::logic_error("The matrix is empty!");
+  }
 
-  if (aRowIdx > theRowsNum)
+  if (aRowIdx > theRowsNum) {
     throw std::out_of_range("Out of range rows!");
+  }
 
-  if (aColIdx > theColumnsNum)
+  if (aColIdx > theColumnsNum) {
     throw std::out_of_range("Out of range columns!");
+  }
 
   return theRows[aRowIdx][aColIdx];
 }
